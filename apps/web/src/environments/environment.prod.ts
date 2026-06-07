6c67e4265381cf4435394730abe5d6774324fa92
Srujan Dharkar <sjdhkar@gmail.com>
2026-06-07 21:09:17 +0530

    config: set Render demo API URL

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Update this URL after deploying the demo-api to Render
-  apiUrl: 'https://clinicalmind-demo-api.onrender.com',
+  apiUrl: 'https://clinicalmind-xd48.onrender.com',
 };
